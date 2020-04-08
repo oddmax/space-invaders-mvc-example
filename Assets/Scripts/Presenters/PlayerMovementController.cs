d9e02e6c4d196797c8cbafdec3d3075faf68c685
Maxim Priakhin <maxim.priakhin@innogames.com>
2020-04-08 16:10:24 +0200

    refactor movement controller

--- Assets/Scripts/Presenters/PlayerMovementController.cs
+++ Assets/Scripts/Presenters/PlayerMovementController.cs
@@ -1,21 +1,22 @@
 using Data;
+using DefaultNamespace.StaticData;
 using UnityEngine;
+using Zenject;
 
 namespace DefaultNamespace.Presenters
 {
 	public class PlayerMovementController : MonoBehaviour
-	{
-        public float speed;
-        public float tilt;              // tilt factor
-        public Boundary boundary;       // movememnt boundary
-
+    {
+        [Inject] 
+        private PlayerConfig playerConfig;
+        
         public GameObject shot;         // bullet prefab
         public Transform shotSpawn;     // the turret (bullet spawn location)
         public Rigidbody myRigidbody;   // reference to rigitbody
-        public float fireRate = 0.5f;   
+        
+        public float smoothing = 5;     // this value is used for smoothing movement
+        
         private float nextFire = 0.0f;
-
-        public float smoothing = 5;     // this value is used for smoothing ovement
         private Vector3 smoothDirection;// used to smooth out mouse and touch control
 
         // Use this for initialization
@@ -26,44 +27,32 @@ namespace DefaultNamespace.Presenters
 
         void FixedUpdate()
         {
-            // keyboard
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 pos = Input.mousePosition;
 
-            // if keyboard direction key is pressed
-            if (moveHorizontal != 0 || moveVertical != 0)
-            {
-                myRigidbody.velocity = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed;
-            }
-            else
-            {
-                Vector3 pos = Input.mousePosition;
+            pos.z = Camera.main.transform.position.y + 1;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            Vector3 origin = new Vector3(transform.position.x, transform.position.y, transform.position.z);
 
-                pos.z = Camera.main.transform.position.y + 1;
-                pos = Camera.main.ScreenToWorldPoint(pos);
-                Vector3 origin = new Vector3(transform.position.x, transform.position.y, transform.position.z);//.zero;
+            Vector2 currentPosition = new Vector3(pos.x, pos.z);
+            Vector3 directionRaw = pos - origin;
 
-                Vector2 currentPosition = new Vector3(pos.x, pos.z);
-                Vector3 directionRaw = pos - origin;
-                Debug.Log("directionRaw.magnitude=" + directionRaw.magnitude);
+            Vector3 direction = directionRaw.normalized;
 
-                Vector3 direction = directionRaw.normalized;
+            smoothDirection = Vector3.MoveTowards(smoothDirection, direction, smoothing);
 
-                smoothDirection = Vector3.MoveTowards(smoothDirection, direction, smoothing);
-
-                direction = smoothDirection;
-                Vector3 movement = new Vector3(direction.x, 0, direction.z);
-                myRigidbody.velocity = movement * speed;
-            }
-
+            direction = smoothDirection;
+            Vector3 movement = new Vector3(direction.x, 0, direction.z);
+            myRigidbody.velocity = movement * playerConfig.movementSpeed;
+            
+            var movementBoundary = playerConfig.movementBoundary;
             transform.position = new Vector3
             (
-                Mathf.Clamp(transform.position.x, boundary.xMin, boundary.xMax),
+                Mathf.Clamp(transform.position.x, movementBoundary.xMin, movementBoundary.xMax),
                 0.0f,
-                Mathf.Clamp(transform.position.z, boundary.zMin, boundary.zMax)
+                Mathf.Clamp(transform.position.z, movementBoundary.zMin, movementBoundary.zMax)
             );
 
-            myRigidbody.rotation = Quaternion.Euler(0, 0, myRigidbody.velocity.x * -tilt);
+            myRigidbody.rotation = Quaternion.Euler(0, 0, myRigidbody.velocity.x * -playerConfig.movementTiltFactor);
         }
 
         void Update()
@@ -71,7 +60,7 @@ namespace DefaultNamespace.Presenters
             // Should we fire a bullet?
             if ((Input.GetButton("Fire1") || Input.GetKeyDown(KeyCode.Space)) && Time.time > nextFire)
             {
-                nextFire = Time.time + fireRate;
+                nextFire = Time.time + playerConfig.fireRate;
                 Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
             }
         }
