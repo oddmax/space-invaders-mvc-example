d3c6b6ad5635ea72e1c986cb761aea089f82e153
Maxim Priakhin <maxim.priakhin@innogames.com>
2020-04-08 23:42:13 +0200

    implement missing info in enemy view

--- Assets/Scripts/Views/EnemyView.cs
+++ Assets/Scripts/Views/EnemyView.cs
@@ -1,9 +1,29 @@
+using Data;
 using UnityEngine;
 
 namespace DefaultNamespace
 {
 	public class EnemyView : MonoBehaviour
 	{
+		[SerializeField]
+		private Transform weaponCoordinates; // the turret (bullet spawn location)
+
+		public EnemyData EnemyData { get; private set; }
+		public Rigidbody Rigidbody; 
+
+		public void Init(EnemyData enemyData)
+		{
+			EnemyData = enemyData;
+		}
 		
+		public Vector3 GetWeaponCoordinates()
+		{
+			return weaponCoordinates.position;
+		}
+		
+		public Quaternion GetRotation()
+		{
+			return weaponCoordinates.rotation;
+		}
 	}
 }
